ae7c00b3838c5806c5e6a76eaf73da3825a0b37d
Abel <90abel.avr@gmail.com>
2023-01-24 21:56:51 -0300

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom"
+const Layout = () => {
+  return (
+    <div>
+        <h1 className="text-6xl font-bold">CRM Pacientes</h1>
+        <Outlet />
+    </div>
+  )
+}
+
+export default Layout
